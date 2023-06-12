767454f48983236e2b81c1f4f1dc787324d5a57d
Marc Anton Dahmen <mail@marcdahmen.de>
2023-06-12 10:52:08 +0200

    refactor fzf-projects

--- .config/fzf/fzf-projects.sh
+++ .config/fzf/fzf-projects.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 fzfProjects() {
-	projectsPath=~/MAD-Dev
-	projects=$(fd '\.git$' ~/MAD-Dev --exclude node_modules --hidden --no-ignore -t d -x dirname {} \; 2>/dev/null)
-	selected=$(printf "$projects\n/Users/marcantondahmen/.config/nvim/" | fzf --no-multi --color=16 --cycle --preview-window=border-left --preview='(cd {1} && [ -f README.* ] && cat README.* || ls -l)')
+	local projects=$(fd '\.git$' ~/MAD-Dev --exclude node_modules --hidden --no-ignore -t d -x dirname {} \; 2>/dev/null)
+	local items="$projects\n/Users/marcantondahmen/.config/nvim/"
+	local preview='(cd {1} && [ -f README.* ] && cat README.* || ls -l)'
+	local selected=$(printf "$items" | fzf --no-multi --color=16 --cycle --preview-window=border-left --preview="$preview")
 
 	if [ ! -z "$selected" ]
 	then
