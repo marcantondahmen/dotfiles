10c2320beee5adbb48373d724f7c973ec14ba0b1
Marc Anton Dahmen <mail@marcdahmen.de>
2023-06-12 11:05:20 +0200

    update fzf defaults

--- .config/fzf/fzf-projects.sh
+++ .config/fzf/fzf-projects.sh
@@ -4,7 +4,7 @@ fzfProjects() {
 	local projects=$(fd '\.git$' ~/MAD-Dev --exclude node_modules --hidden --no-ignore -t d -x dirname {} \; 2>/dev/null)
 	local items="$projects\n/Users/marcantondahmen/.config/nvim/"
 	local preview='(cd {1} && [ -f README.* ] && cat README.* || ls -l)'
-	local selected=$(printf "$items" | fzf --no-multi --color=16 --cycle --preview-window=border-left --preview="$preview")
+	local selected=$(printf "$items" | fzf --no-multi --preview="$preview")
 
 	if [ ! -z "$selected" ]
 	then
--- .config/fzf/fzf.zsh
+++ .config/fzf/fzf.zsh
@@ -4,6 +4,7 @@ if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
 fi
 
 export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_DEFAULT_OPTS="--color=16 --cycle --preview-window=border-left"
 
 # Auto-completion
 [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
@@ -13,5 +14,4 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
 # Custom functions
 source "$(dirname "$0")/fzf-projects.sh"
-
 
