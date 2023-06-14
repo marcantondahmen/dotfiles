3684e2549e03422cf14ed7b8702fe08059c2fb77
Marc Anton Dahmen <mail@marcdahmen.de>
2023-06-14 22:41:43 +0200

    update fzf config

--- .config/fzf/fzf.zsh
+++ .config/fzf/fzf.zsh
@@ -4,7 +4,7 @@ if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
 fi
 
 export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_DEFAULT_OPTS="--color=16 --cycle --preview-window=border-left"
+export FZF_DEFAULT_OPTS="--color=16 --cycle --preview-window=border-sharp"
 
 # Auto-completion
 [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
@@ -13,5 +13,36 @@ export FZF_DEFAULT_OPTS="--color=16 --cycle --preview-window=border-left"
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
 # Custom functions
-source "$(dirname "$0")/fzf-projects.sh"
+fzfProjects() {
+	local projects=$(fd '\.git$' ~/MAD-Dev --exclude node_modules --hidden --no-ignore -t d -x dirname {} \; 2>/dev/null)
+	local items="$projects\n/Users/marcantondahmen/.config/nvim/"
+	local preview='(cd {1} && [ -f README.* ] && bat --color=always --style=numbers --theme=base16 README.* || ls -l)'
+	local selected=$(printf "$items" | fzf --no-multi --preview="$preview")
+
+	if [ ! -z "$selected" ]
+	then
+		cd $selected
+		nvim
+	fi
+}
+
+fzfOpen() {
+	local filePreview='if file -b --mime-type {} | grep -qiE "text|json"; then bat --color=always --style=numbers --theme=base16 {}; else echo "Binary file"; fi'
+	local dirPreview='echo "Directory contents:" && ls -lA --color=always {} | awk '"'"'{print $9}'"'"''
+	local selected=$(fd -H -I -E node_modules -E cache -E .git . $(pwd) | \
+		fzf \
+		--no-multi \
+		--preview="if [ -f {} ]; then $filePreview; else $dirPreview; fi" \
+	)
+
+	if [ ! -z $selected ]
+	then
+		if [ -f "$selected" ]
+		then
+			cd $(dirname "$selected") && nvim "$selected"
+		else
+			cd "$selected" 
+		fi
+	fi
+}
 
